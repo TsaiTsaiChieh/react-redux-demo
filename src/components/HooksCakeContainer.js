aa1bc3916d1c2a155d83e82697931a8ab9ec1c47
Tsai-Chieh <d0177381@gmail.com>
2022-03-24 19:34:11 +0800

    :sparkles: add useSelector, useDispatch

---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {buyCake} from '../redux'
+import {BUY_CAKE} from '../redux/cake/cakeTypes'
+
+function HooksCakeContainer() {
+  const numOfCakes = useSelector(state=> state.numOfCakes)
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <h2>Num of cakes - {numOfCakes}</h2>
+      <button onClick={()=> dispatch({type: BUY_CAKE})}>Buy cake</button>
+      <button onClick={()=> dispatch(buyCake())}>Buy cake</button>
+    </div>
+  )
+}
+
+export default HooksCakeContainer
