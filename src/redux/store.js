2586c9c19142dc0f9c9b864c269bb3bf47c74252
Tsai-Chieh <d0177381@gmail.com>
2022-03-25 11:08:20 +0800

    :sparkles: add redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from "redux"
 import logger from 'redux-logger'
 import { rootReducer } from './rootReducer'
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-export const store = createStore(rootReducer, applyMiddleware(logger))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
